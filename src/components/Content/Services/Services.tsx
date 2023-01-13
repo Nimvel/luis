cf1586c2fca99b85b25c78523529043eee0028cf
Nimvel <rabbit.of.margery@gmail.com>
2023-01-13 16:48:28 +0300

    services

--- src/components/Content/Services/Services.tsx
+++ src/components/Content/Services/Services.tsx
@@ -4,7 +4,34 @@ import s from './Services.module.scss'
 const Services = () => {
     return (
         <div className={s.services}>
-            Services
+            <h2>services</h2>
+            <div className={s.columns}>
+            <div>
+                <div className={`${s.img} ${s.user_research}`} />
+                <p>User Reserch</p>
+            </div>
+            <div>
+                <div className={`${s.img} ${s.ia_and_wireframes}`} />
+                <p>IA And Wireframes</p>
+            </div>
+            <div>
+                <div className={`${s.img} ${s.user_testing}`} />
+                <p>User Testing</p>
+            </div>
+            <div>
+                <div className={`${s.img} ${s.ui}`} />
+                <p>Looks and Feel Design (UI)</p>
+            </div>
+            <div>
+                <div className={`${s.img} ${s.delivery}`} />
+                <p>Delivery</p>
+            </div>
+            <div>
+                <div className={`${s.img} ${s.support}`} />
+                <p>Integration Support</p>
+            </div>
+            </div>
+            
         </div>
     )
 }
